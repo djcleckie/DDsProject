a0273114ac40a8a59f30b0f5d41151e1523e8dad
djcleckie <djcleckie@dive-boats.co.uk>
2022-09-27 23:53:48 +0100

    started adding routines for TFT and Rotary

---
+++ Version15_09-22/ROTARY.cpp
@@ -0,0 +1,55 @@
+#ifndef BEENHERE            // Make sure we only read header file once 
+  #include "DDSvfo.h"
+#endif
+
+
+// Rotary Encoder routines
+
+// this only returns when the bit pattern is as expected
+// it debounces the rotary encoder well
+// A vald CW or  CCW move returns 1 or -1, invalid returns 0
+unsigned int read_rotary() 
+{
+  static unsigned int rot_enc_table[] = {0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0};
+
+  prevNextCode <<= 2;
+  if (digitalRead(pin_A)) prevNextCode |= 0x02;
+  if (digitalRead(pin_B)) prevNextCode |= 0x01;
+  prevNextCode &= 0x0f;
+
+   // If valid then store as 16 bit data.
+   if  (rot_enc_table[prevNextCode] ) {
+      store <<= 4;
+      store |= prevNextCode;
+
+      if ((store&0xff)==0x2b) return -1;
+      if ((store&0xff)==0x17) return 1;
+   }
+   return 0;
+}
+
+void rotary_enc()
+{
+
+  static unsigned int val;
+
+  if( val=read_rotary() ) 
+  {
+    if ( prevNextCode==0x0b) 
+    {
+      TargetFrequency = TargetFrequency + DDSStep;
+      if (TargetFrequency < VFOLowerLimit) 
+      {
+        TargetFrequency = VFOUpperLimit;
+      }
+    }
+    if ( prevNextCode==0x07) 
+    {
+      TargetFrequency = TargetFrequency - DDSStep;
+      if (TargetFrequency > VFOUpperLimit) 
+      {
+        TargetFrequency = VFOLowerLimit;
+      }
+    }
+  }
+}
---
+++ Version15_09-22/TFT.cpp
@@ -0,0 +1,30 @@
+#ifndef BEENHERE            // Make sure we only read header file once 
+  #include "DDSvfo.h"
+#endif
+void TFTinit()
+{
+// define pins for the TFT display
+  // we only need to worry about non SPI pins
+  pinMode(TFT_DC, OUTPUT);  // TFT pins (plus SPI1)
+  pinMode(TFT_CS, OUTPUT);
+  pinMode(TFT_RST, OUTPUT);
+  digitalWrite(TFT_DC, LOW); // internal pull-down
+  digitalWrite(TFT_CS, LOW);
+  digitalWrite(TFT_RST, LOW);  
+}
+void TFTsetup()
+{
+// setup and initialise the screen
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.fillRect(0, 110, 320, 95, ILI9341_RED);
+  tft.setCursor(110, 10);
+  tft.setTextColor(ILI9341_RED);    
+  tft.setTextSize(4);
+  tft.println("GM4NFI");
+  tft.setTextColor(ILI9341_GREEN);  
+  tft.setCursor(55, 60);
+  tft.setTextSize(3);
+  tft.println("DDS VFO");
+}  
